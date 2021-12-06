a1876d5b7d89f5905a18b19c7bd67464ed1dddf6
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-06 11:51:47 -0800

    Refactor methods to not require hardcoded vals

--- 2021/3/3.rb
+++ 2021/3/3.rb
@@ -11,19 +11,19 @@ def parse_file(file)
   end
 end
 
-def update_bit_frequency (byte)
+def update_bit_frequency(byte, frequency_hash)
   byte.gsub("\n","").split('').each_with_index do |bit, index|
-    if @bit_list[index]
-      @bit_list[index] = @bit_list[index] + bit.to_i
+    if frequency_hash[index]
+      frequency_hash[index] = frequency_hash[index] + bit.to_i
     else
-      @bit_list[index] = bit.to_i
+      frequency_hash[index] = bit.to_i
     end
   end
 end
 
-def get_bit_frequency(byte_list)
+def get_bit_frequency(byte_list, bit_hash)
   byte_list.each do |byte|
-    update_bit_frequency (byte)
+    update_bit_frequency(byte, bit_hash)
   end
 end
 
@@ -39,8 +39,17 @@ def get_binary_readings(frequency_list, num_readings)
   end
 end
 
+def get_acceptable_vals(byte_list, bit_criteria)
+
+  bit_freq_list = {}
+
+  byte_list.each do |byte|
+
+  end
+end
+
 parse_file('test_input.txt')
-get_bit_frequency(@array)
+get_bit_frequency(@array, @bit_list)
 get_binary_readings(@bit_list, @array.length.to_f)
 puts "Gamma binary: #{@gamma_binary}. Gamma decimal: #{@gamma_binary.to_i(2)}."
 puts  "Epsilon binary: #{@epsilon_binary}. Epsilon decimal: #{@epsilon_binary.to_i(2)}"
