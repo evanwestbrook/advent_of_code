b48b8a1978eb3e63a4cfc997111267c5fb51a655
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-06 09:11:44 -0800

    refactor for working solution

--- 2021/1/1.rb
+++ 2021/1/1.rb
@@ -1,4 +1,5 @@
 @num_increases = 0
+@sum_array = []
 
 def file_to_array(file)
   read_file = File.readlines(file)
@@ -9,11 +10,18 @@ def file_to_array(file)
   end
 end
 
+def make_sum (index)
+  sum = @array[index] + @array[index + 1] + @array[index + 2]
+end
+
 def get_increases
   @array.each_with_index do |element, index|
-    if index > 0
-      if element > @array[index - 1]
-        @num_increases += 1
+    if index < @array.count - 2
+      @sum_array << make_sum(index)
+      if index > 0
+        if @sum_array[index] > @sum_array[index - 1]
+          @num_increases += 1
+        end
       end
     end
   end
