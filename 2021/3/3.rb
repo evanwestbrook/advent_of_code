19caf3bdd5ec7da29768cd7411b09abaf9c0926a
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-06 10:28:52 -0800

    Set up method to convert count of each bit into a hash

--- 2021/3/3.rb
+++ 2021/3/3.rb
@@ -1,13 +1,43 @@
 # gamma rate is most common bit
 # epsilon rate is least commit bit. Can just be the reverse of gamma
 
+@bit_list = {}
+@gamma_binary = ""
+@epsilon_binary = ""
+
 def parse_file(file)
   read_file = File.readlines(file)
   @array = []
 
   read_file.each do |line|
-    puts line
+    @array << line
+  end
+end
+
+def split_byte (byte)
+  byte.gsub("\n","").split('').each_with_index do |bit, index|
+    if @bit_list[index]
+      @bit_list[index] = @bit_list[index] + bit.to_i
+    else
+      @bit_list[index] = bit.to_i
+    end
+  end
+end
+
+def get_file_stats
+  @array.each do |byte|
+    split_byte (byte)
+  end
+end
+
+def get_binary_readings
+  @bit_list.each_with_index do |bit, index|
+    puts "Bit: #{bit}. Index: #{index}"
   end
 end
 
 parse_file('test_input.txt')
+get_file_stats
+#puts @bit_list
+get_binary_readings
+#puts @array.length
