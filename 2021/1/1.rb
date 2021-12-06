4d83a625cfa2cfbf59a63919ea57224cf6a1f86b
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-06 08:52:56 -0800

    Complete solution

--- 2021/1/1.rb
+++ 2021/1/1.rb
@@ -1,3 +1,5 @@
+@num_increases = 0
+
 def file_to_array(file)
   read_file = File.readlines(file)
   @array = []
@@ -7,4 +9,16 @@ def file_to_array(file)
   end
 end
 
+def get_increases
+  @array.each_with_index do |element, index|
+    if index > 0
+      if element > @array[index - 1]
+        @num_increases += 1
+      end
+    end
+  end
+end
+
 file_to_array('input.txt')
+get_increases
+puts @num_increases
