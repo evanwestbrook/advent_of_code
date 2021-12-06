7451c6672e25b516b0dd91e053190f45cef80656
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-06 09:33:58 -0800

    Add working solution for part 1

--- 2021/2/2.rb
+++ 2021/2/2.rb
@@ -1,14 +1,32 @@
+@horizontal = 0
+@depth = 0
+
+
 def parse_file(file)
   read_file = File.readlines(file)
   @array = []
 
   read_file.each do |line|
     parsed_line = line.split
-    dict = {command: parsed_line[0], unit: parsed_line[1]}
-    puts dict
+    dict = {command: parsed_line[0], unit: parsed_line[1].to_i}
     @array << dict
   end
 end
 
-parse_file('test_input.txt')
-puts @array
+def plot_course
+  @array.each do |command|
+    if command[:command] == "forward"
+      @horizontal += command[:unit]
+    elsif command[:command] == "down"
+      @depth += command[:unit]
+    elsif command[:command] == "up"
+      @depth -= command[:unit]
+    end
+  end
+end
+
+
+
+parse_file('input.txt')
+plot_course
+puts "Horizontal position: #{@horizontal}. Depth: #{@depth}. Multiply: #{@horizontal * @depth}."
