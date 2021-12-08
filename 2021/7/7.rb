faf01d80e11fdda65ed555db94bb1dd446b3fb0d
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-08 09:12:25 -0800

    Complete part 1 solution

--- 2021/7/7.rb
+++ 2021/7/7.rb
@@ -1,4 +1,5 @@
 @crabs = []
+@crab_frequencies = {}
 
 def parse_input(file)
   read_file = File.readlines(file)
@@ -6,6 +7,33 @@ def parse_input(file)
   read_file[0].split(",").each do |i|
     @crabs << i.to_i
   end
+end
+
+def get_crab_frequency(crabs)
+  crabs.each do |crab|
+    if @crab_frequencies[crab]
+      @crab_frequencies[crab] += 1
+    else
+      @crab_frequencies[crab] = 1
+    end
+  end
+end
+
+# This solution assumes it will be optimal if at least one group of crabs does not move.
+# It does not consider scenarios where some crabs are very far away and all crabs moving
+# could be more efficient.
+def process_fuel_scenarios
+  fuel_scenarios = {}
+  @crab_frequencies.each do |key, value|
+    fuel_scenarios[key] = calculate_fuel(key, @crabs)
+  end
+
+  return fuel_scenarios
+end
+
+def find_min_fuel(fuel_scenarios)
+  fuel_scenarios = fuel_scenarios.sort_by { |key, value| value }
+  return { fuel: fuel_scenarios[0][1], position: fuel_scenarios[0][0]}
 end
 
 def calculate_fuel(position, crabs)
@@ -17,9 +45,10 @@ def calculate_fuel(position, crabs)
   return fuel_consumption
 end
 
-# parse crab coordinates into freqency table and then find the most commons?
-# But if there was a really big one, then it could make sense for all the smalelr ones to move?
+parse_input('input.txt')
 
-parse_input('test_input.txt')
-puts "#{@crabs}"
-puts calculate_fuel(2, @crabs)
+get_crab_frequency(@crabs)
+fuel_scenarios = process_fuel_scenarios
+min_fuel = find_min_fuel(fuel_scenarios)
+puts "Minimum Fuel Position: #{min_fuel[:position]}"
+puts "Minimum Fuel Cost: #{min_fuel[:fuel]}"
