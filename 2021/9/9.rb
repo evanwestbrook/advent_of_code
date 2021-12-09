48a70494e01ffee8f68195170f6b26e148a5e684
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-09 11:45:55 -0800

    Update file parsing to handle weird data issue where last index would not be included on last line

--- 2021/9/9.rb
+++ 2021/9/9.rb
@@ -7,10 +7,19 @@
 def parse_input(file)
   read_file = File.readlines(file)
 
-  read_file.each do |row|
+  read_file.each_with_index do |row, index|
+    puts "row: #{row}"
     row_heights = []
-    (row.length - 1).times do |i|
-      row_heights << row[i].to_i
+    if index == read_file.length - 1
+      (row.length).times do |i|
+        puts row[i].to_i
+        row_heights << row[i].to_i
+      end
+    else
+      (row.length - 1).times do |i|
+        puts row[i].to_i
+        row_heights << row[i].to_i
+      end
     end
 
     @rows << row_heights
@@ -30,6 +39,7 @@ def get_top(row_index, col_index)
 end
 
 def get_bottom(row_index, col_index)
+  puts "#{row_index} #{col_index} #{@rows[row_index + 1][col_index]}"
   return @rows[row_index + 1][col_index]
 end
 
@@ -69,6 +79,7 @@ end
 
 def check_mid_row(height, row_index, col_index)
   puts "height: #{height}"
+  puts "Row index: #{row_index}, Col index: #{col_index}, End index: #{@rows[row_index].length}"
   if col_index == 0
     right = get_right(row_index, col_index)
     top = get_top(row_index, col_index)
@@ -156,6 +167,8 @@ end
 
 def find_low_points
   @rows.each_with_index do |row, index|
+    puts "Index: #{index}"
+    puts "Row: #{row}"
     find_row_low_points(row, index)
   end
 end
@@ -170,10 +183,17 @@ def calc_risk_level
   return risk_level
 end
 
-parse_input('test_input.txt')
-#puts "#{@rows}"
-find_low_points
+parse_input('input.txt')
 #find_row_low_points(@rows[2], 2)
+
+find_low_points
 puts "#{@low_points}"
 puts "#{calc_risk_level}"
+#puts "Row: #{@rows[99].length}"
+
+#Row index: 98, Col index: 99, End index: 100
+#98 99
+#left: 9 top: 9 bottom:
+#Index: 98
+#Row: [2, 1, 2, 3, 4, 5, 6, 7, 9, 8, 8, 8, 9, 4, 3, 1, 2, 3, 4, 9, 8, 9, 9, 9, 8, 4, 3, 2, 1, 4, 3, 4, 5, 6, 9, 8, 9, 8, 9, 7, 6, 9, 5, 4, 0, 1, 2, 3, 4, 5, 9, 5, 4, 3, 4, 9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 9, 9, 1, 2, 3, 4, 5, 6, 7, 9, 8, 5, 4, 8, 8, 9, 8, 7, 6, 5, 4, 5, 6, 7, 8, 9, 2, 3, 9, 8, 9, 2]
 
