a1bd3406f25f12263b09e6be1a896015bddb242f
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-11 11:21:30 -0800

    Add first pass at energy indexing method

--- 2021/11/11.rb
+++ 2021/11/11.rb
@@ -8,6 +8,17 @@
 # Problem: after 100 steps, how many flashes?
 
 @rows = []
+@numflashes = 0
+
+def print_grid
+  @rows.each do |row|
+    row_string = ""
+    row.each do |col|
+      row_string += col.to_s
+    end
+    puts row_string
+  end
+end
 
 def parse_input(file)
   read_file = File.readlines(file)
@@ -104,29 +115,136 @@ def get_adjacent(row_index, col_index)
 end
 
 def step(energy, row_index, col_index)
-  puts "Starting energy: #{energy}"
+  #puts "Starting energy: #{energy}"
   if energy >= 9
     # Handle flashing
-    puts "flash"
+    #puts "flash"
     # Initialize flashes
     # In this recursive scenario, we keep track of the points we have already considered in our basin.
     #   In this manner, we do not re-evaluate points that we have already considered.
-    basin = {}
-    basin[low_point[:row_index].to_s + "_" + low_point[:col_index].to_s] = low_point[:height]
+    flashes = {}
+
+    handle_flash(flashes, energy, row_index, col_index)
   else
     energy += 1
     @rows[row_index][col_index] = energy
+    #puts "Ending energy: #{energy}"
   end
-  puts "Ending energy: #{energy}"
 end
 
-def flash(flashes, energy)
+def handle_flash(flashes, energy, row_index, col_index)
+  # Add coordinates to the flashes already handled by this flash and reset energy levels for next flash
+  flashes[row_index.to_s + "_" + col_index.to_s] = energy
+  @rows[row_index][col_index] = 0
+  #puts flashes
+  #puts "Ending energy: #{0}"
+  @numflashes += 1
+  flash(flashes, energy, row_index, col_index)
+end
+
+def flash(flashes, energy, row_index, col_index)
+  # Determine valid adjacent energy levels
+  adjacent_energy = get_adjacent(row_index, col_index)
+  #puts "#{adjacent_energy}"
+
+  # Recursively evalute potential flashes
+  if adjacent_energy[:w] < 99
+    if adjacent_energy[:w] >= 9
+      handle_flash(flashes, adjacent_energy[:w], row_index, col_index - 1)
+    else
+      #puts "new energy west: #{adjacent_energy[:w] + 1}"
+      @rows[row_index][col_index - 1] = adjacent_energy[:w] + 1
+    end
+  end
+  if adjacent_energy[:e] < 99
+    if adjacent_energy[:e] >= 9
+      handle_flash(flashes, adjacent_energy[:e], row_index, col_index + 1)
+    else
+      #puts "new energy east: #{adjacent_energy[:e] + 1}"
+      @rows[row_index][col_index + 1] = adjacent_energy[:e] + 1
+    end
+  end
+  if adjacent_energy[:n] < 99
+    if adjacent_energy[:n] >= 9
+      handle_flash(flashes, adjacent_energy[:n], row_index - 1, col_index)
+    else
+      #puts "new energy north: #{adjacent_energy[:n] + 1}"
+      @rows[row_index - 1][col_index] = adjacent_energy[:n] + 1
+    end
+  end
+  if adjacent_energy[:s] < 99
+    if adjacent_energy[:s] >= 9
+      handle_flash(flashes, adjacent_energy[:s], row_index + 1, col_index)
+    else
+      #puts "new energy south: #{adjacent_energy[:s] + 1}"
+      @rows[row_index + 1][col_index] = adjacent_energy[:s] + 1
+    end
+  end
+  if adjacent_energy[:nw] < 99
+    if adjacent_energy[:nw] >= 9
+      handle_flash(flashes, adjacent_energy[:nw], row_index - 1, col_index - 1)
+    else
+      #puts "new energy northwest: #{adjacent_energy[:nw] + 1}"
+      @rows[row_index - 1][col_index - 1] = adjacent_energy[:nw] + 1
+    end
+  end
+  if adjacent_energy[:ne] < 99
+    if adjacent_energy[:ne] >= 9
+      handle_flash(flashes, adjacent_energy[:ne], row_index - 1, col_index + 1)
+    else
+      #puts "new energy northeast: #{adjacent_energy[:ne] + 1}"
+      @rows[row_index - 1][col_index + 1] = adjacent_energy[:ne] + 1
+    end
+  end
+  if adjacent_energy[:sw] < 99
+    if adjacent_energy[:sw] >= 9
+      handle_flash(flashes, adjacent_energy[:sw], row_index + 1, col_index - 1)
+    else
+      #puts "new energy southwest: #{adjacent_energy[:sw] + 1}"
+      @rows[row_index + 1][col_index - 1] = adjacent_energy[:sw] + 1
+    end
+  end
+  if adjacent_energy[:se] < 99
+    if adjacent_energy[:se] >= 9
+      handle_flash(flashes, adjacent_energy[:se], row_index + 1, col_index + 1)
+    else
+      #puts "new energy southeast: #{adjacent_energy[:se] + 1}"
+      @rows[row_index + 1][col_index + 1] = adjacent_energy[:se] + 1
+    end
+  end
+end
+
+def process_row(row, row_index)
+  row.each_with_index do |col, index|
+    step(@rows[col][index], col, index)
+    #check_point(col, row_index, index)
+  end
+end
+
+def process_rows
+  @rows.each_with_index do |row, index|
+    puts "Row ##{index}"
+    print_grid
+
+    process_row(row, index)
+  end
+end
+
+def process_steps(num_steps)
+  num_steps.times do |num_step|
+    puts "Step ##{num_step}"
+    process_rows
+  end
 end
 
 puts "----- STARTING -----"
 parse_input('test_input.txt')
 #puts "#{@rows}"
 #puts get_adjacent(1, 1)
+#puts "Step 1"
+#step(@rows[0][2], 0, 2)
+#puts "Step 2"
+#step(@rows[0][2], 0, 2)
 
-step(@rows[0][2], 0, 2)
-step(@rows[0][2], 0, 2)
+process_steps(1)
+puts "# of flashes: #{@numflashes}"
