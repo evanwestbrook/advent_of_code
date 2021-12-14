7d75544c344c12b6f579f181df1a0aacfe62e354
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-14 09:51:43 -0800

    Update step logic to use polymer_pair hash for steps

--- 2021/14/14.rb
+++ 2021/14/14.rb
@@ -6,7 +6,6 @@
 # For scoring, still evaluate keys against element freqency. This way, we're still evaluting the string
 
 @insertion_rules = {}
-@polymer = ""
 @polymer_pairs = {}
 @element_frequency = {}
 
@@ -25,9 +24,7 @@ def parse_input(file)
         update_possible_elements(row[i])
         # Initialize element pairs in polymer
         if !(i == row.length - 1)
-          if !@polymer_pairs[row[i] + row[i + 1]]
-            @polymer_pairs[row[i] + row[i + 1]] = 1
-          end
+          update_polymer_pairs(row[i] + row[i + 1], @polymer_pairs)
         end
       end
     end
@@ -43,29 +40,45 @@ def parse_input(file)
   end
 end
 
+def update_polymer_pairs(element, polymer_pairs)
+  if !polymer_pairs[element]
+    polymer_pairs[element] = 1
+  else
+    polymer_pairs[element] += 1
+  end
+end
+
 def update_possible_elements(element)
   if !@element_frequency[element]
     @element_frequency[element] = 0
   end
 end
 
-def update_polymer(polymer)
-  updated_polymer = ""
-  polymer.length.times do |element|
-    if !(element == polymer.length - 1)
-      updated_polymer += polymer[element]
-      updated_polymer += @insertion_rules[polymer[element] + polymer[element + 1]]
-    else
-      updated_polymer += polymer[element]
+def insert_polymer(polymer_pair)
+  new_polymers = []
+  new_polymers << polymer_pair[0] + @insertion_rules[polymer_pair]
+  new_polymers << @insertion_rules[polymer_pair] + polymer_pair[0]
+
+  puts new_polymers
+end
+
+def update_polymer(polymer_pairs)
+  updated_polymer_pairs = {}
+
+  polymer_pairs.each do |polymer_pair|
+    new_polymers = [polymer_pair[0][0] + @insertion_rules[polymer_pair[0]], @insertion_rules[polymer_pair[0]] + polymer_pair[0][1]]
+
+    new_polymers.each do |new_polymer|
+      update_polymer_pairs(new_polymer, updated_polymer_pairs)
     end
   end
 
-  return updated_polymer
+  return updated_polymer_pairs
 end
 
 def step_polymer(steps)
   steps.times do |step|
-    @polymer = update_polymer(@polymer)
+    @polymer_pairs = update_polymer(@polymer_pairs)
   end
 end
 
@@ -87,6 +100,8 @@ puts "----- Starting -----"
 parse_input('test_input.txt')
 puts @polymer_pairs
 puts @element_frequency
-#step_polymer(10)
+#insert_polymer("NN")
+step_polymer(2)
+puts @polymer_pairs
 
 #puts "The solution to part 2 is: #{get_polymer_score(@polymer)}"
