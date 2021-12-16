95693d5c182a3f55667d7d052f9e77ef343158cf
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-15 16:23:16 -0800

    Convert day 1 solution to complete enumerable methods

--- 2021/1/1.rb
+++ 2021/1/1.rb
@@ -1,27 +1,11 @@
-@num_increases = 0
-@sum_array = []
-
-def make_sum (index)
-  sum = @array[index] + @array[index + 1] + @array[index + 2]
-end
-
-def get_increases
-  @array.each_with_index do |element, index|
-    if index < @array.count - 2
-      @sum_array << make_sum(index)
-      if index > 0
-        if @sum_array[index] > @sum_array[index - 1]
-          @num_increases += 1
-        end
-      end
-    end
-  end
-end
+# Lessons learned from converting to all Enuemrable methods
+# 1. For parsing an input file into an array, collect does a lot of work for us
+#    a. Map does something similar, and I'm still not sure of the differences
+# 2. Each consecutive works well for comparing items in an array similar to how a .each_with_index would work.
+#    However, it is a little easier to ready, as there are fewer instances where we are juggling indices
+# 3. Inject was not as useful here as a solution because we are not doing something cumulative. The solution relies
+#    on items from previous aspects of the array
 
 @array = File.readlines('input.txt').collect { |line| line.gsub("\n", '').to_i}
-#puts "#{@array}"
-
-#file_to_array_2('small_input.txt')
-#file_to_array('test_input.txt')
-get_increases
-puts @num_increases
+puts "Part 1 solution: #{@array.each_cons(2).select { |x| x[0] < x[1] }.count}"
+puts "Part 2 solution: #{@array.each_cons(3).map(&:sum).each_cons(2).select { |x| x[0] < x[1] }.count}"
