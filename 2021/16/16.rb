4b2afd443b57b5e91aa6715eb3aa4ad0ea2eec9e
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-16 15:18:49 -0800

    Get help and finish part 2

--- 2021/16/16.rb
+++ 2021/16/16.rb
@@ -1,10 +1,9 @@
-# Note: Had to get help based on this solution: https://github.com/erikw/advent-of-code-solutions/blob/main/2021/day16/part1.rb
-# It was approaching things in a similar way to how I did initially, but it seemed to work.
-# I was having issues with my parsed array returning nested in such a manner that I couldn't parse it on subsequent
-# recursions. I was having a tough time figuring it out, so I went and got help.
-# This solution method works a little easier because it keeps consuming the values instead of trying to keep track of where they are.
-# Ultimately, live tracking all of the items and then summing them (my initial solution) proved to be a little too
-# complicated to keep track of. 
+# Note: Had to get help based on this solution: https://github.com/erikw/advent-of-code-solutions/blob/main/2021/day16/part2.rb
+# Ultimately, it was frustratings, as the original way I was doing things (storing in the array) for processing
+# later is how this solution was approached.
+# In the future, I think the lesson learned was that I was approaching it the correct way, but I was approaching it
+# in a more complicated manner. Instead of keeping the entire string, I could keep consuming it. There were also
+# better ways that I could iterate over the messages while processing.
 
 def parse_decoder(file_name)
   hex_decoder = {}
@@ -32,30 +31,31 @@ end
 
 def decode_operator_l(digits, numbits)
   start_length = digits.length
-  versions = 0
+  values = []
 
   while start_length - digits.length < numbits
-    version = decode_transmission(digits)
-    break if version.nil?
+    value = decode_transmission(digits)
+    break if value.nil?
 
-    versions += version
+    values << value
   end
 
-  return versions
+  return values
 end
 
 def decode_operator_n(digits, nopacks)
   packs = 0
-  versions = 0
+  values = []
   while packs < nopacks
 
-    v = decode_transmission(digits)
-    break if v.nil?
+    value = decode_transmission(digits)
+    break if value.nil?
 
-    versions += v
+    values << value
     packs += 1
   end
-  versions
+
+  return values
 end
 
 def decode_literal(digits)
@@ -69,29 +69,45 @@ def decode_literal(digits)
   binary.to_i(2)
 end
 
+def decode_subpacks(digits)
+  length_type_id = consume_digits(digits, 1).to_i(2)
+
+  if length_type_id == 0
+    length = consume_digits(digits, 15).to_i(2)
+    decode_operator_l(digits, length)
+  else
+    length = consume_digits(digits, 11).to_i(2)
+    decode_operator_n(digits, length)
+  end
+end
+
 def decode_transmission(digits)
   return nil if digits.length < 11
 
-  versions = consume_digits(digits, 3).to_i(2)
+  version = consume_digits(digits, 3).to_i(2)
   type_id = consume_digits(digits, 3).to_i(2)
   case type_id
+  when 0
+    decode_subpacks(digits).sum
+  when 1
+    decode_subpacks(digits).inject(&:*)
+  when 2
+    decode_subpacks(digits).min
+  when 3
+    decode_subpacks(digits).max
   when 4
     literal_value = decode_literal(digits)
-  else
-    length_type_id = consume_digits(digits, 1).to_i(2)
-    if length_type_id == 0
-      length = consume_digits(digits, 15).to_i(2)
-      versions += decode_operator_l(digits, length)
-    else
-      length = consume_digits(digits, 11).to_i(2)
-      versions += decode_operator_n(digits, length)
-    end
+  when 5
+    decode_subpacks(digits).each_cons(2).all? { |a, b| a > b } ? 1 : 0
+  when 6
+    decode_subpacks(digits).each_cons(2).all? { |a, b| a < b } ? 1 : 0
+  when 7
+    decode_subpacks(digits).each_cons(2).all? { |a, b| a == b } ? 1 : 0
   end
-  versions
 end
 
 puts "===== STARTING ====="
 @hex_decoder = parse_decoder('hex_decoder.txt')
 transmission = decode_hex(File.readlines('input.txt')[0].gsub("\n", ''))
 
-puts "Sum of Versions: #{decode_transmission(transmission)}"
+puts "Values: #{decode_transmission(transmission)}"
