9c6ed07a33b6ad930f0d4e185768a3644a05c232
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-17 09:41:39 -0800

    Refactor trajectory logic into Probe class

--- 2021/17/17.rb
+++ 2021/17/17.rb
@@ -1,3 +1,5 @@
+require './probe.rb'
+
 def parse_input(file)
   File.readlines(file).each do |row|
     instructions = row.gsub("\n", '')
@@ -7,56 +9,14 @@ def parse_input(file)
   end
 end
 
-def step(coords, velocity)
-  puts "Coodinates: #{coords}"
-  puts "Velocity: #{velocity}"
-  x_res = step_x(coords[0], velocity[0])
-  y_res = step_y(coords[1], velocity[1])
-  hit = check_target([x_res[:x_coord], y_res[:y_coord]])
-  return {
-    coords: [x_res[:x_coord], y_res[:y_coord]],
-    velocity: [x_res[:x_velocity], y_res[:y_velocity]],
-    hit: hit
-  }
-end
-
-def step_x(x_coord, x_velocity)
-
-  x_coord += x_velocity
-
-  if x_velocity > 0
-    x_velocity -= 1
-  end
-  return { x_coord: x_coord, x_velocity: x_velocity }
-end
-
-def step_y(y_coord, y_velocity)
-  y_coord += y_velocity
-  return { y_coord: y_coord, y_velocity: y_velocity -= 1 }
-end
-
-def check_target(coords)
-  if coords[0] >= @x_range[0] && coords[0] <= @x_range[1]
-    if coords[1] >= @y_range[0] && coords[1] <= @y_range[1]
-      return true
-    end
-  end
-
-  return false
-end
-
 def fire_probe(velocity)
-  coords = [0,0]
+  probe = Probe.new([0,0], velocity)
 
   loop do
-    res = step(coords, velocity)
-    if res[:hit]
-      p res
-      return "hit!"
+    probe.move
+    if probe.hit?(@x_range, @y_range)
+      probe.print_info
       break
-    else
-      coords = res[:coords]
-      velocity = res[:velocity]
     end
   end
 end
---
+++ 2021/17/probe.rb
@@ -0,0 +1,41 @@
+class Probe
+  def initialize(coordinates, velocity)
+    @x_coord = coordinates[0]
+    @y_coord = coordinates[1]
+    @x_velocity = velocity[0]
+    @y_velocity = velocity[1]
+  end
+
+  def print_info
+    puts "Coordinates: #{@x_coord} , #{@y_coord}"
+    puts "Velocity: #{@x_velocity} , #{@y_velocity}"
+  end
+
+  def move
+    step_x
+    step_y
+  end
+
+  def step_x
+    @x_coord += @x_velocity
+
+    if @x_velocity > 0
+      @x_velocity -= 1
+    end
+  end
+
+  def step_y
+    @y_coord += @y_velocity
+    @y_velocity -= 1
+  end
+
+  def hit?(x_target_range, y_target_range)
+    if @x_coord >= x_target_range[0] && @x_coord <= x_target_range[1]
+      if @y_coord >= y_target_range[0] && @y_coord  <= y_target_range[1]
+        return true
+      end
+    end
+
+    return false
+  end
+end
