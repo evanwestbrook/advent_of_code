b9373da8295d5b33bdba62e59e281db874ad4067
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-17 13:57:10 -0800

    Attempt coordinate increasing patterns

--- 2021/17/17.rb
+++ 2021/17/17.rb
@@ -3,6 +3,7 @@ require './probe.rb'
 # Start at lowest height we could be at (with the minimum x?)
 # keep backing off x and increasing y until we miss
 # one right before that will be the solution
+@hit_velocities = []
 
 def parse_input(file)
   File.readlines(file).each do |row|
@@ -29,51 +30,74 @@ def fire_probe(velocity)
   end
 end
 
-def find_starting_x
-  velocity = [1,0]
+def find_all_velocities
+  starting_velocity = [0 - @y_range.max + 1, 1] # range at which top left is hit
+  puts "Starting: velocity #{starting_velocity}"
+  @hit_velocities << starting_velocity
+
+  get_velocities(starting_velocity)
+
+  #increase y velocity unitl miss
+  #reduce yvelocity until miss
+  # increas x velocity and repeat
+
+end
+
+def get_velocities(velocity)
+  puts "Velocity: #{velocity}"
+  increase_velocities_y(velocity)
+  decrease_velocities_y(velocity)
+
+  inc_x_v = velocity.dup
+  inc_x_v = [inc_x_v[0] + 1, inc_x_v[1]]
+  probe = fire_probe(inc_x_v)
+  if probe[:hit]
+    @hit_velocities << inc_x_v
+    get_velocities(inc_x_v)
+  #else
+  #elsif probe[:probe].x_coord < @x_range.max
+    puts probe[:probe].x_coord
+  #  get_velocities(inc_x_v)
+  end
+end
+
+def increase_velocities_y(velocity)
+  inc_v = velocity.dup
+
+  inc_v = [inc_v[0], inc_v[1] + 1]
 
   loop do
-    if fire_probe(velocity)[:hit]
-      return velocity
+    probe = fire_probe(inc_v)
+    if probe[:hit]
+      @hit_velocities << inc_v
+      inc_v = [inc_v[0], inc_v[1] + 1]
     else
-      velocity = [velocity[0] + 1, velocity[1]]
+      break
     end
   end
 end
 
-def find_max_y_velocity
-  #starting_x = 0 - @y_range.max + 1
-  velocity = find_starting_x
-  max_y = 0
-  best_velocity = velocity
+def decrease_velocities_y(velocity)
+  inc_v = velocity.dup
+
+  inc_v = [inc_v[0], inc_v[1] - 1]
 
   loop do
-    probe_results = fire_probe(velocity)
-
-    if probe_results[:hit]
-      if probe_results[:probe].max_y >= max_y
-        max_y = probe_results[:probe].max_y
-        best_velocity = velocity
-        velocity = [velocity[0], velocity[1] + 1]
-      else
-        return best_velocity
-      end
-    else # handle misses
-      # if miss x increment x
-      # if miss y increment y
-      velocity = [velocity[0] + 1 , velocity[1] - 1]
-      #return [velocity[0], velocity[1] - 1]
+    probe = fire_probe(inc_v)
+    if probe[:hit]
+      @hit_velocities << inc_v
+      inc_v = [inc_v[0], inc_v[1] - 1]
+    else
+      break
     end
   end
 end
 
 puts "===== STARTING ====="
-parse_input('input.txt')
+parse_input('test_input.txt')
 puts "X range: #{@x_range}"
 puts "Y range: #{@y_range}"
-max_velocity = find_max_y_velocity
-
-puts "Velocity for max y: #{max_velocity}"
-puts "The max y for this velocity is: #{fire_probe(max_velocity)[:probe].max_y}"
-
-puts fire_probe([20,85])
+find_all_velocities
+#puts "Hit velocities #{@hit_velocities}"
+#puts "Hit velocities #{@hit_velocities.length}"
+puts @x_range.max
