3b346e4810d7d0e79619faa77bc4f674be262c79
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-18 09:51:43 -0800

    Add split logic

--- 2021/18/18.rb
+++ 2021/18/18.rb
@@ -12,8 +12,20 @@ def add(left, right)
   return [left, right]
 end
 
-parse_input('test_input_6.txt')
-#puts "#{@snailfish_numbers}"
+def split(regular_number)
+  return [(regular_number.to_f / 2).floor.to_i, (regular_number.to_f / 2).ceil.to_i]
+end
 
-puts "#{add(eval("[1,2]"), eval("[[3,4],5]"))}"
+def explode(exploder, snailfish_number)
+end
+
+parse_input('test_input_6.txt')
+
+test_num = eval("[[[[[9,8],1],2],3],4]")
+test_exploder = eval("[9,8]")
+#puts "#{test_num[0][0][0][0]}"
+
+#explode(test_exploder, test_num)
+
+puts "#{split(11)}"
 
