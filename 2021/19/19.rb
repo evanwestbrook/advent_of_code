8bc82d331f9a741d476b44c764389236c04b7a04
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-19 10:23:34 -0800

    Correct file parsing to include last scanner

--- 2021/19/19.rb
+++ 2021/19/19.rb
@@ -1,15 +1,20 @@
+# Iterate over beacons
+# find overlaps in beacons
+# find distinc beacons >> answer
 def parse_input(file)
   @scanners = []
   scanner = {}
   scanner_num = 0
-  File.readlines(file).each_with_index do |row, index|
+  file_lines = File.readlines(file)
+
+  file_lines.each_with_index do |row, index|
     row = row.gsub(/\n/, "")
     if row.include? "---"
       if index > 0
         @scanners << scanner
       end
       scanner = {}
-      scanner_num = 0
+      #scanner_num = 0
       row = row.split(" ")
       scanner_num = row[2].to_i
       scanner[scanner_num] = []
@@ -17,6 +22,10 @@ def parse_input(file)
       row = row.split(",")
       row = row.map{ |coord| coord = coord.to_i}
       scanner[scanner_num] << row
+    end
+
+    if index == file_lines.length - 1
+      @scanners << scanner
     end
   end
 end
