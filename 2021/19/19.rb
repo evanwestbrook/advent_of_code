36024ed228182ab437c1a663768f0ebbd8e29eae
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-20 09:09:46 -0800

    Get solution with significant help

--- 2021/19/19.rb
+++ 2021/19/19.rb
@@ -1,93 +1,105 @@
-#https://github.com/mebeim/aoc/blob/master/2021/original_solutions/day19.py
-#https://www.reddit.com/r/adventofcode/comments/rjpf7f/comment/hp5nnej/?utm_source=share&utm_medium=web2x&context=3
+# https://www.reddit.com/r/adventofcode/comments/rjpf7f/comment/hp844vn/?utm_source=share&utm_medium=web2x&context=3
+# https://gist.github.com/vincentwoo/dd1136e151c9035e090dd6401f301dcf
 
-require './matrix_math.rb'
+require 'matrix'
+require 'set'
+
+def permutations position
+  (0..2).flat_map do |rot_idx|
+    pos = position.rotate(rot_idx)
+    [
+      [ pos[0],  pos[1],  pos[2]],
+      [ pos[0], -pos[1], -pos[2]],
+      [ pos[0], -pos[2],  pos[1]],
+      [ pos[0],  pos[2], -pos[1]],
+
+      [-pos[0],  pos[1], -pos[2]],
+      [-pos[0], -pos[2], -pos[1]],
+      [-pos[0], -pos[1],  pos[2]],
+      [-pos[0],  pos[2],  pos[1]],
+    ]
+  end.map { |pos| Vector[*pos] }
+end
+
+def find_offset s0, s1
+  s1[0..13].product(s0).each do |s1_elem, s0_elem|
+    offset = s0_elem - s1_elem
+    return offset if s1.map { |e| e + offset }.intersection(s0).length >= 12
+  end
+end
+
+def permute_by pos, perm_idx
+  perms = permutations [1, 2, 3]
+  perms[perm_idx].map { |i| i < 0 ? -pos[(-i) - 1] : pos[i - 1] }
+end
 
 def parse_input(file)
-  @scanners = []
-  scanner = []
-  file_lines = File.readlines(file)
+  input = File.read(file)
 
-  file_lines.each_with_index do |row, index|
-    row = row.gsub(/\n/, "")
-    if row.include? "---"
-      if index > 0
-        @scanners << scanner
-      end
-      scanner = []
-    elsif row.include? ","
-      row = row.split(",")
-      row = row.map{ |coord| coord = coord.to_i}
-      scanner << row
-    end
+  scanners = input.split("\n\n").map { |scanner|
+    scanner
+      .split("\n")[1..-1]
+      .map { |line| line.split(',').map(&:to_i) }
+      .map { |pos| permutations pos }.transpose
+  }
 
-    if index == file_lines.length - 1
-      @scanners << scanner
-    end
-  end
+  return scanners
 end
 
-@MATCH_THRESHOLD = 12
-
-def common_points(s1, s2, i1, i2)
-  @bases.each do |basis|
-    new_s2 = []
-    s2.each do |vec|
-      new_s2 << basis_transform(vec, basis)
-    end
-
-   s1.product(new_s2).each do |point|
-    dist = diff(point[1], point[1])
-
-    translated_s2 = []
-    new_s2.each do |p|
-      translated_s2 << add(p, dist)
-      if (sl + translated_s2).length >= MATCH_THRESHOLD
-        return [translated_s2, dist]
+def get_fingerprints(scanners)
+  scanner_fingerprints = scanners.map { |scanner_perms|
+    scanner_perms.map do |perm|
+      perm.map do |elem|
+        (perm - [elem]).map { |_elem| elem - _elem }.min_by &:magnitude
       end
     end
-   end
-  end
+  }
 
-  return [nil, nil]
+  return scanner_fingerprints
 end
 
-
-
-puts "===== STARTING ====="
-@bases = get_bases
-
-
-
-parse_input('./data/input.txt')
-
-matched = {"0": @scanners[0]}
-distances = [0, 0, 0]
-done = []
-
-loop do
-  @scanners.each_with_index do |scanner, i |
-    if done.include?(scanner) || !matched.include?(scanner)
-      @scanners.each_with_index do |sub_scanner, j|
-        if matched.include? sub_scanner || scanner == sub_scanner
-          common_points = common_points(matched[i], scanners[j], scanner, sub_scanner)
-
-          if common_points[0]
-            matched[j] = common_points[0]
-            distances << common_points[1]
-          end
-
-        end
-      end
-
-      done << scanner
-
+def get_mappings(scanners, scanner_fingerprints)
+  mappings = (0...scanners.length).to_a.permutation(2).map {|a, b|
+    s0 = scanner_fingerprints[a][0]
+    s1 = scanner_fingerprints[b]
+    if perm_idx = s1.find_index { |_s1| _s1.intersection(s0).length >= 12 }
+      [a, b, perm_idx,
+        find_offset(scanners[a][0], scanners[b][perm_idx])
+      ]
     end
-  end
+  }.compact
 
-  if done.length == @scanners.length
-    break
-  end
+  return mappings
 end
 
-p matched[:"0"].length
+def find_scanner_positions(scanners, mappings)
+  scanner_positions = [Vector[0, 0, 0]]
+  canonicalized_beacons = scanners[0][0]
+  transform_chain = {0 => []}
+  until transform_chain.length == scanners.length
+    mapping = mappings.find { |_s0_idx, _s1_idx|
+      transform_chain.include?(_s0_idx) &&
+      !transform_chain.include?(_s1_idx)
+    }
+
+    s0_idx, s1_idx, perm_idx, offset = mapping
+    transform_chain[s1_idx] = [[perm_idx, offset]] + transform_chain[s0_idx]
+
+    s1 = [offset] + scanners[s1_idx][perm_idx].map { |elem| elem + offset }
+    transform_chain[s0_idx].each do |perm_idx, offset|
+      s1.map! { |elem| permute_by(elem, perm_idx) + offset }
+    end
+    scanner_positions.push s1.shift
+    canonicalized_beacons = (canonicalized_beacons + s1).uniq
+  end
+
+  return {beacons: canonicalized_beacons, scanners: scanner_positions}
+end
+
+@scanners = parse_input('./data/input.txt')
+@scanner_fingerprints = get_fingerprints(@scanners)
+@scanner_mappings = get_mappings(@scanners, @scanner_fingerprints)
+@equipment_locations =  find_scanner_positions(@scanners, @scanner_mappings)
+
+puts "There are #{@equipment_locations[:beacons].length} beacons"
+puts "The largest Manhattan distance between two sscnners is #{@equipment_locations[:scanners].combination(2).map { |a, b| (b-a).map(&:abs).sum }.max}"
