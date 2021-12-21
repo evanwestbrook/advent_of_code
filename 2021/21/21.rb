de42855f5d8ecd811364abf932f88f1e59c6e4cc
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-21 09:45:15 -0800

    Update die class to track times rolled

--- 2021/21/21.rb
+++ 2021/21/21.rb
@@ -5,11 +5,6 @@ die = DeterministicDie.new
 player1 = Player.new(4)
 player2 = Player.new(8)
 
-puts "P1 Starting position: #{player1.position}"
-puts "P1 Starting score: #{player1.score}"
-puts "P2 Starting position: #{player2.position}"
-puts "P2 Starting score: #{player2.score}"
-
 4.times do |index|
   player1.take_turn(die)
   puts "P1 #{index} position: #{player1.position}"
--- 2021/21/deterministic_die.rb
+++ 2021/21/deterministic_die.rb
@@ -1,9 +1,12 @@
 class DeterministicDie
+  attr_accessor :num_rolls
   def initialize
     @value = 0
+    @num_rolls = 0
   end
 
   def roll
+    @num_roll += 1
     if @value < 100
       @value += 1
     else
