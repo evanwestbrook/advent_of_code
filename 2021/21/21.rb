5ab2661ac90e1a2976049edab045def7439a3a2d
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-22 08:30:51 -0800

    Complete Part 2 with help

--- 2021/21/21.rb
+++ 2021/21/21.rb
@@ -30,43 +30,6 @@ def play_deterministic_game
   puts "Solution:#{loser.score * die.num_rolls}"
 end
 
-def count_wins_2(player, player1, player2)
-
-  if check_score(player1, player2)
-    return check_score(player1, player2)
-  end
-
-  wins = [0, 0]
-
-  @dirac_rolls.each do |roll|
-    if player.player_num == 0
-      player1.take_turn_dirac(roll.sum())
-      new_player = Player.new(player1.position, player1.score, 0)
-      count_res = count_wins(player2, new_player, player2)
-    else
-      player2.take_turn_dirac(roll.sum())
-      new_player = Player.new(player2.position, player2.score, 1)
-      count_res = count_wins(player1, player1, new_player)
-    end
-
-    wins[0] += count_res[0]
-    wins[1] += count_res[1]
-  end
-
-  p wins
-
-
-  return wins
-end
-
-def check_score(player1, player2)
-  if player1.score >= 21
-    return [1, 0]
-  elsif player2.score >= 21
-    return [1, 0]
-  end
-end
-
 def get_dirac_rolls
   die = DiracDie.new(1)
 
@@ -104,57 +67,64 @@ def get_dirac_rolls
   return dirac_sums
 end
 
-def play_dirac_game
-  @dirac_rolls = get_dirac_rolls
-  @won = [0, 0]
+# trying this solution
+# https://github.com/HrRodan/adventofcode2021/blob/master/day21/day21_part2.py
 
-  player1 = Player.new(10, 0, 0)
-  player2 = Player.new(4, 0, 1)
-  wins = count_wins(player1, player1, player2)
-  p wins
-  p wins.max()
+def move_player(position, score, steps)
+  position_final = (position + steps - 1) % 10 + 1
+  return position_final, score + position_final
 end
 
+def roll_next(position, score, length, p, length_dict)
+  @dirac_rolls.each do |roll_sum, count|
+    if score >= 21
+      return
+    end
+    new_position, new_score = move_player(position, score, roll_sum.to_i)
+    new_length = length + 1
+    p_new = p * count
 
-def play(pos, score, roll)
-  new_pos = ((pos - 1 + roll) % 10) + 1
-  new_score = score + new_pos
-  return new_pos, new_score
-end
-
-# The following is from this solution but did not work well for me
-#https://www.reddit.com/r/adventofcode/comments/rl6p8y/2021_day_21_solutions/hph4r3k/?utm_source=share&utm_medium=web2x&context=3
-def count_wins(player, pos0, score0, pos1, score1)
-  if score0 >= 21
-    return 1, 0
-  elsif score1 >= 21
-    return 0, 1
-  end
-
-  wins = [0,0]
-
-  @dirac_rolls.each do |key, value|
-    @i +=1
-    p @i
-    if player == 0
-      new_pos, new_score = play(pos0, score0, key.to_i)
-      wins0, wins1 = count_wins(1, new_pos, new_score, pos1, score1)
+    if length_dict[new_length.to_s + "_" + new_score.to_s]
+      length_dict[new_length.to_s + "_" + new_score.to_s] += p_new
     else
-      new_pos, new_score = play(pos1, score1, key.to_i)
-      wins0, wins1 = count_wins(0, pos0, score0, new_pos, new_score)
+      length_dict[new_length.to_s + "_" + new_score.to_s] = p_new
     end
 
-    wins[0] += wins0
-    wins[1] += wins1
+    roll_next(new_position, new_score, new_length, p_new, length_dict)
   end
-
-  return wins
 end
 
 @dirac_rolls = get_dirac_rolls
 
-p @dirac_rolls
+player_positions = [3, 10]
+length_dicts = []
 
-@i = 0
+# Get all possibilities for a given number
+player_positions.each do |player_position|
+  d = {}
+  roll_next(player_position, 0, 0, 1, d)
+  length_dicts.append(d)
+end
 
-p count_wins(0, 10, 0, 4, 0)
+won = [0,0]
+
+length_dicts[0].each do |length1_score1, value1|
+  l1s1split = length1_score1.to_s.split("_")
+  length1 = l1s1split[0].to_i
+  score1 = l1s1split[1].to_i
+
+  length_dicts[1].each do |length2_score2, value2|
+    l2s2split = length2_score2.to_s.split("_")
+    length2 = l2s2split[0].to_i
+    score2 = l2s2split[1].to_i
+
+    if score1 >= 21 && length2 == length1 - 1 && score2 < 21
+      won[0] += value1 * value2
+    end
+    if score2 >= 21 && length2 == length1 - 1 && score1 < 21
+      won[1] += value1 * value2
+    end
+  end
+end
+
+puts "The # of universes in which the most player with the most wins wins is #{won.max()}"
