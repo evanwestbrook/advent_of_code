b524323d2077132db616337555f3fbd4fae33dfa
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-23 10:58:35 -0800

    Add part notes for hand solving

--- 2021/23/23.rb
+++ 2021/23/23.rb
@@ -1,11 +1,1 @@
-@energy = {
-  "A": 1,
-  "B": 10,
-  "C" 100,
-  "D" 1000
-}
-
-# Part 1
-# Solved it manually on paper...
-# Part 2
-# Almost solved manually on paper...
+#Solved part 2 using https://amphipod.net/ by hand
