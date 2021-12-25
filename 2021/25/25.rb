4a6b6f676364af3b05d6b67f106ba7cf21378cca
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-25 10:05:42 -0800

    Initialize day 25

---
+++ 2021/25/25.rb
@@ -0,0 +1,7 @@
+# Each step
+# east try to move
+# south try to move
+# Can only move if htere is an open space in front of it
+# right boundary moves to the left boundary
+# bottom boundary moves to teh top boundary
+# Solution: find when sea cucumbers stop moving (all blocked)
