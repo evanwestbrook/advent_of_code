5376b0e5105ab12c449d885d032dfc19b74c6c53
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-25 11:09:44 -0800

    Add safe landing evaluation

--- 2021/25/25.rb
+++ 2021/25/25.rb
@@ -35,6 +35,7 @@ def print_cucumbers(sea_cucumbers)
 end
 
 def step_east(sea_cucumbers)
+  num_moves = 0
   sea_cucumbers.each do |cucumber_row|
     # Duplicate row for comparison so cucumbers only move 1 space per step
     old_row = cucumber_row.dup
@@ -47,19 +48,24 @@ def step_east(sea_cucumbers)
           if old_row[0] == "."
             cucumber_row[0] = ">"
             cucumber_row[index] = "."
+            num_moves += 1
           end
         else
           if old_row[index + 1] == "."
             cucumber_row[index + 1] = ">"
             cucumber_row[index] = "."
+            num_moves += 1
           end
         end
       end
     end
   end
+
+  return num_moves
 end
 
 def step_south(sea_cucumbers)
+  num_moves = 0
   num_cols = sea_cucumbers[0].length
   num_rows = sea_cucumbers.length
 
@@ -80,25 +86,42 @@ def step_south(sea_cucumbers)
           if my_col[0] == "."
             sea_cucumbers[0][i] = "v"
             sea_cucumbers[index][i] = "."
+            num_moves += 1
           end
         else
           if my_col[index + 1] == "."
             sea_cucumbers[index + 1][i] = "v"
             sea_cucumbers[index][i] = "."
+            num_moves += 1
           end
         end
       end
     end
+  end
+  return num_moves
+end
+
+def step_cucumbers(sea_cucumbers)
+  num_moves = 0
+  num_moves += step_east(sea_cucumbers)
+  num_moves += step_south(sea_cucumbers)
+  return num_moves
+end
+
+def find_landing(sea_cucumbers)
+  step_num = 1
+  loop do
+    num_moves = step_cucumbers(sea_cucumbers)
+    if num_moves == 0
+      puts "It is safe to land after #{step_num} steps."
+      break
+    end
+
+    step_num += 1
   end
 end
 
 puts "===== STARTING ====="
 
 @sea_cucumbers = parse_input('./data/test.txt')
-print_cucumbers(@sea_cucumbers)
-puts "STEP 1 a"
-step_east(@sea_cucumbers)
-print_cucumbers(@sea_cucumbers)
-puts "STEP 1 b"
-step_south(@sea_cucumbers)
-print_cucumbers(@sea_cucumbers)
+find_landing(@sea_cucumbers)
