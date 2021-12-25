66896a908dda5521ff82d37b3a12b31fd3d3bd24
Evan Westbrook <evan.westbrook@stitchfix.com>
2021-12-25 10:09:18 -0800

    Add file parsing logic

--- 2021/25/25.rb
+++ 2021/25/25.rb
@@ -5,3 +5,22 @@
 # right boundary moves to the left boundary
 # bottom boundary moves to teh top boundary
 # Solution: find when sea cucumbers stop moving (all blocked)
+
+def parse_input(file)
+  sea_cucumbers = []
+  File.readlines(file).each do |row|
+    row = row.gsub(/\n/, "")
+    row = row.split("")
+    cucumber_row = []
+    row.each do |cucumber|
+      cucumber_row << cucumber
+    end
+
+    sea_cucumbers << cucumber_row
+
+  end
+
+  return sea_cucumbers
+end
+
+p parse_input('./data/test.txt')
