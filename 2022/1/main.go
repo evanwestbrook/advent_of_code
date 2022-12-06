088409a3546d5bcba43a07b74693b2e7abe61340
Evan Westbrook <91969396+evanwestbrook@users.noreply.github.com>
2022-12-06 11:14:39 -0800

    Complete Day 1 Part 2

--- 2022/1/main.go
+++ 2022/1/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -12,6 +13,46 @@ func main() {
 	fmt.Println("Day 1")
 
 	part1("./input_1_data.txt")
+	part2("./input_1_data.txt")
+}
+
+func part2(filePath string) {
+	fmt.Println("Part 2:")
+	lines, err := parseFile(filePath)
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	var ec []int
+	var cc int = 0
+
+	for i, line := range lines {
+		if line == "" {
+			ec = append(ec, cc)
+			cc = 0
+		} else if i == len(lines) - 1{
+			lv, err := strconv.Atoi(line)
+			if err != nil {
+				log.Fatalf("strconv: %s", err)
+			}
+			cc = cc + lv
+			ec = append(ec, cc)
+			cc = 0
+		} else {
+			lv, err := strconv.Atoi(line)
+			if err != nil {
+				log.Fatalf("strconv: %s", err)
+			}
+			cc = cc + lv
+		}
+	}
+
+	sort.Sort(sort.IntSlice(ec))
+	sort.Slice(ec, func(i, j int) bool {
+		return ec[i] > ec[j]
+  })
+
+	fmt.Println(sum(ec[:3]))
 }
 
 func part1(filePath string) {
@@ -56,4 +97,13 @@ func parseFile(filePath string) ([]string, error) {
 	}
 
 	return lines, scanner.Err()
+}
+
+func sum(array[] int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+
+	return result
 }
