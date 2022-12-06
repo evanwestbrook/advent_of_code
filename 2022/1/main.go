6bfd285929961200e83a6435dd03d15b81944147
Evan Westbrook <91969396+evanwestbrook@users.noreply.github.com>
2022-12-06 10:39:10 -0800

    Add file parsing function

--- 2022/1/main.go
+++ 2022/1/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Day 1")
+
+	lines, err := parseFile("./test_data.txt")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	fmt.Println(lines)
+
+}
+
+func parseFile(filePath string) ([]string, error) {
+	f, err := os.Open(filePath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
 }
