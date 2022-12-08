2e7ab40c6cd3be068799396f46f25193f99d4a7f
Evan Westbrook <91969396+evanwestbrook@users.noreply.github.com>
2022-12-08 14:54:02 -0800

    Complete Day 8 Part 2

--- 2022/8/main.go
+++ 2022/8/main.go
@@ -11,7 +11,8 @@ import (
 func main() {
 	fmt.Println("Day 8")
 
-	part1("./input_data.txt")
+	part1("./test_data.txt")
+	part2("./test_data.txt")
 }
 
 func part1(filePath string) {
@@ -39,6 +40,110 @@ func part1(filePath string) {
 	fmt.Println(totalVisible)
 }
 
+func part2(filePath string) {
+	fmt.Println("Part 2")
+	// Read file
+	lines, err := parseFile(filePath)
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	treeGrid := prepData(lines)
+
+	maxScenicScore := 0
+
+	for i := 0; i < len(treeGrid[0]) - 1; i++ {
+		for j := 0; j < len(treeGrid) - 1; j++ {
+			currScenicScore := getScenicScore(i, j, treeGrid)
+			if currScenicScore > maxScenicScore {
+				maxScenicScore = currScenicScore
+			}
+		}
+	}
+
+	fmt.Println(maxScenicScore)
+}
+
+func getScenicScore(x_coord int, y_coord int, treeGrid [][]string) int {
+	scenicScore := topVisibleCount(x_coord, y_coord, treeGrid)
+	scenicScore	=	scenicScore * bottomVisibleCount(x_coord, y_coord, treeGrid)
+	scenicScore	=	scenicScore * leftVisibleCount(x_coord, y_coord, treeGrid)
+	scenicScore	=	scenicScore * rightVisibleCount(x_coord, y_coord, treeGrid)
+
+	return scenicScore
+}
+
+func rightVisibleCount(x_coord int, y_coord int, treeGrid [][]string) int {
+	currXCoord := x_coord
+	numVisible := 0
+
+	for i := 0; i < len(treeGrid[0]) - x_coord - 1; i++ {
+		compTreeHeight := treeGrid[y_coord][currXCoord + 1]
+		if compTreeHeight >= treeGrid[y_coord][x_coord] {
+			numVisible = numVisible + 1
+			return numVisible
+		} else {
+			currXCoord = currXCoord + 1
+			numVisible = numVisible + 1
+		}
+	}
+
+	return numVisible
+}
+
+func leftVisibleCount(x_coord int, y_coord int, treeGrid [][]string) int {
+	currXCoord := x_coord
+	numVisible := 0
+
+	for i := 0; i < x_coord; i++ {
+		compTreeHeight := treeGrid[y_coord][currXCoord - 1]
+		if compTreeHeight >= treeGrid[y_coord][x_coord] {
+			numVisible = numVisible + 1
+			return numVisible
+		} else {
+			currXCoord = currXCoord - 1
+			numVisible = numVisible + 1
+		}
+	}
+
+	return numVisible
+}
+
+func bottomVisibleCount(x_coord int, y_coord int, treeGrid [][]string) int {
+	currYCoord := y_coord
+	numVisible := 0
+
+	for i := 0; i < len(treeGrid) - y_coord - 1; i++ {
+		compTreeHeight := treeGrid[currYCoord + 1][x_coord]
+		if compTreeHeight >= treeGrid[y_coord][x_coord] {
+			numVisible = numVisible + 1
+			return numVisible
+		} else {
+			currYCoord = currYCoord + 1
+			numVisible = numVisible + 1
+		}
+	}
+
+	return numVisible
+}
+
+func topVisibleCount(x_coord int, y_coord int, treeGrid [][]string) int {
+	currYCoord := y_coord
+	numVisible := 0
+
+	for i := 0; i < y_coord; i++ {
+		compTreeHeight := treeGrid[currYCoord - 1][x_coord]
+		if compTreeHeight >= treeGrid[y_coord][x_coord] {
+			numVisible = numVisible + 1
+			return numVisible
+		} else {
+			currYCoord = currYCoord - 1
+			numVisible = numVisible + 1
+		}
+	}
+
+	return numVisible
+}
 
 func checkVisibility(x_coord int, y_coord int, treeGrid [][]string) bool {
 		if topVisible(x_coord, y_coord, treeGrid) {
