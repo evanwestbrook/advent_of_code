1e8c8c00034dc3247b8edf6ae1618eaeb6a213ab
Evan Westbrook <91969396+evanwestbrook@users.noreply.github.com>
2022-12-09 11:21:02 -0800

    Add functionality to parse line into move

--- 2022/9/main.go
+++ 2022/9/main.go
@@ -1,9 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Day 9")
+	part1("./test.txt")
+}
+
+type Move struct {
+	direction string
+	steps int
+}
+
+func part1(filePath string) {
+	fmt.Println("Part 1")
+	// Read file
+	lines, err := parseFile(filePath)
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	fmt.Println(getMove(lines[0]))
+}
+
+func getMove(line string) Move{
+	splitLine := strings.Split(line," ")
+	dir := splitLine[0]
+	steps, _ := strconv.Atoi(splitLine[1])
+
+	return Move{dir, steps}
+}
+
+func parseFile(filePath string) ([]string, error) {
+	f, err := os.Open(filePath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
 }
